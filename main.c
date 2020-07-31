f70aa45e683df9298df29a85a513ab1542360a86
zkerriga <daniil.63@yandex.ru>
2020-07-31 10:16:35 +0300

    Add main and libary

---
+++ main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <zkerriga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/31 10:00:03 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/07/31 10:00:10 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*heroes_list_create(void)
+{
+	t_list	*heroes;
+
+	heroes = ft_lstnew(NULL);
+	ft_lstadd_front(&heroes, ft_lstnew(NULL));
+	ft_lstadd_front(&heroes, ft_lstnew(NULL));
+	return (heroes);
+}
+
+t_list	*dragons_list_create(void)
+{
+	t_list	*dragons;
+
+	dragons = ft_lstnew(NULL);
+	ft_lstadd_front(&dragons, ft_lstnew(NULL));
+	ft_lstadd_front(&dragons, ft_lstnew(NULL));
+	ft_lstadd_front(&dragons, ft_lstnew(NULL));
+	ft_lstadd_front(&dragons, ft_lstnew(NULL));
+	return (dragons);
+}
+
+int		main(void)
+{
+	t_list	*dragons;
+	t_list	*heroes;
+
+	dragons = dragons_list_create();
+	heroes = heroes_list_create();
+	return (0);
+}
